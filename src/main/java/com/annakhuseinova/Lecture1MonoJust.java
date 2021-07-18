e0887b9ad0b058e3a431e250874f3f16dba5b3df
Anna Khuseinova <annakhuseinova@yandex.ru>
2021-07-18 16:25:55 +0300

    Initial commit

---
+++ src/main/java/com/annakhuseinova/Lecture1MonoJust.java
@@ -0,0 +1,14 @@
+package com.annakhuseinova;
+
+import reactor.core.publisher.Mono;
+
+public class Lecture1MonoJust {
+
+    public static void main(String[] args) {
+
+        // publisher
+        Mono<Integer> integerMono = Mono.just(1);
+
+        // In reactive programming nothing happens until you subscribe
+    }
+}
