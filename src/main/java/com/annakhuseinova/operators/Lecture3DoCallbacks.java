a7425fad94b8d5c40e58c82a245cc7c80ead1ea2
Anna Khuseinova <annakhuseinova@yandex.ru>
2021-07-24 13:23:22 +0300

    More code

--- src/main/java/com/annakhuseinova/operators/Lecture3DoCallbacks.java
+++ src/main/java/com/annakhuseinova/operators/Lecture3DoCallbacks.java
@@ -19,16 +19,31 @@ public class Lecture3DoCallbacks {
             fluxSink.complete();
             System.out.println("Completed");
         })
+                // behavior triggered when the Flux completes successfully.
                 .doOnComplete(()-> System.out.println("doOnComplete"))
-                .doFirst(()-> System.out.println("doFirst"))
+                // behavior that will be triggered before the Flux is subscribed to,
+                // which should be the first event after assembly time.
+                .doFirst(()-> System.out.println("doFirst 1"))
+                // add behavior triggered when the Flux emits an item
                 .doOnNext(item -> System.out.println("doOnNext: " + item))
+                // add behavior triggered when the Flux is being subscribed, that is to say when a Subscription
+                // has been produced by the Publisher and is being passed to the Subscriber.onSubscribe(subscription)
                 .doOnSubscribe(subscription -> System.out.println("doOnSubscribe"  + subscription))
+                // add behavior triggering a LongConsumer when this Flux receives any request
                 .doOnRequest(l -> System.out.println("doOnRequest :" + l))
+                // add behavior triggered when the Flux completes with an error matching the given exception type
                 .doOnError(error -> System.out.println("doOnError: " + error.getMessage()))
+                // add behavior triggered when the Flux terminates, either by completing successfully or failing with
+                // an error
                 .doOnTerminate(()-> System.out.println("doOnTerminate"))
+                // add behavior triggered when the Flux is cancelled
                 .doOnCancel(()-> System.out.println("doOnCancel"))
+                // add behavior triggered after the Flux terminates for any reason, including cancellation
                 .doFinally(signalType -> System.out.println("doFinally: " + signalType))
+                // Potentially modify the behavior of the whole chain of operators upstream of this one to conditionally
+                // clean up elements that get discarded by these operators
                 .doOnDiscard(Object.class, o -> System.out.println("doOnDiscard :" + o))
+                .doFirst(()-> System.out.println("Do First 2"))
                 .subscribe(Util.subscriber());
     }
 }
